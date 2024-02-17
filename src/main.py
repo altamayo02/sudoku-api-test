7d979adc35ebcf39cd4d00827dec155f5efe1129
Alejandro <42224066+altamayo02@users.noreply.github.com>
2024-02-17 14:28:11 -0500

    Split routes for cell checking and line checking

--- src/main.py
+++ src/main.py
@@ -4,16 +4,7 @@ from model.Sudoku import Sudoku
 app = Flask(__name__)
 sudoku = Sudoku()
 
-
-@app.route("/sudokugame", methods=["GET"])
-def ver_sudoku():
-	print(f"\n{sudoku}")
-	return jsonify({
-		"board": sudoku.board,
-		"draft": sudoku.draft
-	}), 200
-
-@app.route("/sudokugame", methods=["POST"])
+@app.route("/sudokugame/cell", methods=["POST"])
 def validar_jugada():
 	jugada: dict = request.get_json()
 	
@@ -22,7 +13,43 @@ def validar_jugada():
 		fila = jugada["fila"] - 1
 		col = jugada["columna"] - 1
 		num = jugada["numero"]
-		if sudoku.is_valid(fila, col, num):
+		if sudoku.in_cell(fila, col, num):
+			resuelto = sudoku.fill(fila, col, num)
+			print(f"\n{sudoku}")
+			if resuelto:
+				return jsonify({
+					"message": "¡Ha rellenado el sudoku!",
+					"board": sudoku.board,
+					"draft": sudoku.draft
+				}), 200
+			else:
+				return jsonify({
+					"message": "¡La jugada es válida!",
+					"board": sudoku.board,
+					"draft": sudoku.draft
+				}), 200
+		else:
+			return jsonify({
+				"message": (
+					"La jugada no es válida. " +
+					"Asegúrese también de no escribir sobre los números ya dados."
+				)
+			}), 400
+	else:
+		return jsonify({
+			"message": "La fila, la columna y el número deben estar entre 1 y 9."
+		}), 400
+
+@app.route("/sudokugame/lines", methods=["POST"])
+def validar_jugada():
+	jugada: dict = request.get_json()
+	
+	# Si cada atributo de jugada está entre 1 y 9
+	if all([attr in range(1, 10) for attr in jugada.values()]):
+		fila = jugada["fila"] - 1
+		col = jugada["columna"] - 1
+		num = jugada["numero"]
+		if sudoku.in_col(fila, col, num) and sudoku.in_row(fila, col, num):
 			resuelto = sudoku.fill(fila, col, num)
 			print(f"\n{sudoku}")
 			if resuelto:
